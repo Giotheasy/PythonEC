ec5c9f8e13f9ddf91c12ae2bc0778de382917002
Galo Pocoma <Giotheasy@github.com>
2021-08-07 12:15:23 -0400

    Finished first point

--- Cap1/Exercise1-1.py
+++ Cap1/Exercise1-1.py
@@ -1,4 +1,31 @@
 import pandas as pd
+from bokeh.plotting import show
+from bokeh.models import ColumnDataSource, DataTable, TableColumn
 
-data = pd.read_csv('../db/Table 1_3.csv', skiprows=1, header=0, sep=';')
-print(data)
+def inflation(df: pd.DataFrame):
+    return df.pct_change() * 100
+
+
+def readData(path: str):
+    return pd.read_csv(path, skiprows=1, header=0, sep=';', decimal=',', index_col=0)
+
+
+def showTable(df: pd.DataFrame):
+    source = ColumnDataSource(df)
+    columns = [TableColumn(field=col, title=col) for col in df.columns]
+    dataTable = DataTable(source=source, columns=columns, index_position=0)
+    show(dataTable)
+
+def plotInflation():
+    pass
+
+
+def index_a():
+    path: str = '../db/Table 1_3.csv'
+    inf_df = inflation(readData(path))
+    print(inf_df.to_markdown())
+
+if __name__ == '__main__':
+    path: str = '../db/Table 1_3.csv'
+    inf_df = inflation(readData(path))
+    # res_df = (df - df.iloc[5]) / df.iloc[5]
